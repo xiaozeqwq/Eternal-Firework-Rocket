75d4189c05457bdb0ea73c70f20bddd886206bfb
xiaozeqwq <439657748@qq.com>
2026-04-09 16:04:59 +0800

    Adds eternal firework rockets that can be found in End City chests

---
+++ src/client/java/xiaoze_qwq_/eternal_firework_rocket/EternalFireworkRocketDataGenerator.java
@@ -0,0 +1,11 @@
+package xiaoze_qwq_.eternal_firework_rocket;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EternalFireworkRocketDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
